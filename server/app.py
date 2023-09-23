fcfb1c94a4380105e833c2c9fea8c113123170ac
Denzel <jdenzelmab@gmail.com>
2023-09-23 19:22:12 -0400

    Completed authorization lab

--- server/app.py
+++ server/app.py
@@ -83,17 +83,25 @@ class CheckSession(Resource):
             return user.to_dict(), 200
         
         return {}, 401
+    
+
+@app.before_request
+def check_if_logged_in():
+    if not session.get('user_id') \
+        and request.endpoint in ['member_index', 'member_article'] :
+        return {'error': 'Unauthorized'}, 401
 
 class MemberOnlyIndex(Resource):
-    
     def get(self):
-        pass
+        articles = Article.query.filter(Article.is_member_only == True).all()
+        return [article.to_dict() for article in articles], 200
+    
 
 class MemberOnlyArticle(Resource):
-    
     def get(self, id):
-        pass
-
+        articles = Article.query.filter(Article.is_member_only == True).all()
+        return [article.to_dict() for article in articles], 200
+        
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(IndexArticle, '/articles', endpoint='article_list')
 api.add_resource(ShowArticle, '/articles/<int:id>', endpoint='show_article')
